7cc887d808ea21ceb218643e1786040fb5517bf5
Albert Lv <altair.albert@yahoo.com>
2022-08-15 00:03:12 +0800

    feat: use material theme

--- docs/js/mathjax.js
+++ docs/js/mathjax.js
@@ -25,18 +25,3 @@ var _hmt = _hmt || [];
   var s = document.getElementsByTagName("script")[0];
   s.parentNode.insertBefore(hm, s);
 })();
-
-// google analytics
-
-(function () {
-  var hm = document.createElement("script");
-  hm.src = "https://www.googletagmanager.com/gtag/js?id=G-LPSL3YGLRT";
-  var s = document.getElementsByTagName("script")[0];
-  s.parentNode.insertBefore(hm, s);
-  window.dataLayer = window.dataLayer || [];
-  function gtag() {
-    dataLayer.push(arguments);
-  }
-  gtag("js", new Date());
-  gtag("config", "G-LPSL3YGLRT");
-})();
